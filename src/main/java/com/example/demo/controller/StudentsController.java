150b1d26c84ce6330c8a0c6fc99c5b2da2f3ee71
unknown <nestorovicmilan1@gmail.com>
2021-05-11 11:36:48 +0200

    JPA, Entity

--- src/main/java/com/example/demo/controller/StudentsController.java
+++ src/main/java/com/example/demo/controller/StudentsController.java
@@ -1,2 +1,28 @@
-package com.example.demo.controller;public class StudentsController {
+package com.example.demo.controller;
+
+import com.example.demo.model.Student;
+import com.example.demo.service.StudentsService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.time.LocalDate;
+import java.util.List;
+
+@RestController
+@RequestMapping("api/students")
+public class StudentsController {
+    private final StudentsService studentsService;
+
+    @Autowired
+    public StudentsController(StudentsService studentsService) {
+        this.studentsService = studentsService;
+    }
+
+    @GetMapping
+    public List<Student> getStudents() {
+        return studentsService.getStudents();
+    }
+
 }
--- src/main/java/com/example/demo/service/StudentsService.java
+++ src/main/java/com/example/demo/service/StudentsService.java
@@ -1,2 +1,19 @@
-package com.example.demo.service;public class StudentsService {
+package com.example.demo.service;
+
+import com.example.demo.model.Student;
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.time.LocalDate;
+import java.util.List;
+
+@Service
+public class StudentsService {
+    public List<Student> getStudents() {
+        return List.of(
+                new Student(1, "Milan", "milan@ex.com", 23, LocalDate.now()),
+                new Student(1, "Milan", "milan@ex.com", 23, LocalDate.now()),
+                new Student(1, "Milan", "milan@ex.com", 23, LocalDate.now())
+        );
+    }
 }
