b32875b46f99a5e3a665817542edf7c0bfb11882
unknown <nestorovicmilan1@gmail.com>
2021-05-11 12:32:12 +0200

    JPA, Entity, Repository

--- src/main/java/com/example/demo/controller/StudentsController.java
+++ src/main/java/com/example/demo/controller/StudentsController.java
@@ -1,6 +1,7 @@
 package com.example.demo.controller;
 
 import com.example.demo.model.Student;
+import com.example.demo.repository.StudentRepository;
 import com.example.demo.service.StudentsService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
--- src/main/java/com/example/demo/service/StudentsService.java
+++ src/main/java/com/example/demo/service/StudentsService.java
@@ -1,6 +1,8 @@
 package com.example.demo.service;
 
 import com.example.demo.model.Student;
+import com.example.demo.repository.StudentRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.GetMapping;
 
@@ -9,11 +11,19 @@ import java.util.List;
 
 @Service
 public class StudentsService {
+    private final StudentRepository studentRepository;
+
+    @Autowired
+    public StudentsService(StudentRepository studentRepository) {
+        this.studentRepository = studentRepository;
+    }
+
     public List<Student> getStudents() {
-        return List.of(
-                new Student(1, "Milan", "milan@ex.com", 23, LocalDate.now()),
-                new Student(1, "Milan", "milan@ex.com", 23, LocalDate.now()),
-                new Student(1, "Milan", "milan@ex.com", 23, LocalDate.now())
-        );
+//        return List.of(
+//                new Student(1, "Milan", "milan@ex.com", 23, LocalDate.now()),
+//                new Student(1, "Milan", "milan@ex.com", 23, LocalDate.now()),
+//                new Student(1, "Milan", "milan@ex.com", 23, LocalDate.now())
+//        );
+        return studentRepository.findAll();
     }
 }
